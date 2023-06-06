fa0734ad65bb13fa52040c15703080609a1e3f73
josua <josuasiagian827@gmail.com>
2023-06-06 18:57:47 +0700

    fead:update all features

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,10 +6,25 @@ toggle.addEventListener('click', () =>{
     toggle.checked ? html.classList.add('dark') & body.classList.add('bg-black') : html.classList.remove('dark') & body.classList.remove('bg-black');
 }); 
 
+// scrolling
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 20) {
+    navbar.style.backgroundColor = 'slategray';
+  } else {
+    navbar.style.background = 'transparent';
+  }
+});
+
 // darkMode
 const icons = document.querySelector('#icon-darkMode');
-icons.addEventListener('click', () => {
-    icons.classList.toggle('bx-sun');
+icons.addEventListener('click', () =>{
+  icons.classList.toggle('bx-sun');
+});
+const theme = document.querySelector('#theme');
+theme.addEventListener('click', () =>{
+  icons.classList.toggle('bx-sun');
 });
 
 // Mini Menu
@@ -18,7 +33,18 @@ const menu = document.querySelector('.flex-nav');
 menuToggle.addEventListener('click', () =>{
   menuToggle.classList.toggle('bx-x');
   menuToggle.classList.toggle('bx-menu');
+  navbar.style.backgroundColor = 'slategray';
   menuToggle.style.transition = 'all 2s ease';
 
   menu.classList.toggle('active');
+});
+
+// remove active if link click
+const links = document.querySelectorAll('#menuSlider li a');
+links.forEach(link => {
+  link.addEventListener('click', () =>{
+    menu.classList.remove('active');
+    menuToggle.classList.remove('bx-x');
+    menuToggle.classList.toggle('bx-menu');  
+  });
 });
